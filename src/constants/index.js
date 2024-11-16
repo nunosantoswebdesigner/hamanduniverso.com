3f2ce427c22aba2a848cf55ba647664680c30c23
Nuno Santos <42045537+nunosantoswebdesigner@users.noreply.github.com>
2024-11-16 09:24:39 +0000

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,297 @@
+// Imagens
+import { cliente1, cliente2, cliente3, cliente4, cliente5, cliente6, logoSmall, chromecast, disc02, discordBlack, facebook, file02, homeSmile, instagram,  plusSquare, recording01, recording03, serviceOne, serviceTwo, serviceThree, serviceFour, searchMd, sliders04, telegram, tiktok, yourlogo, clienteCard, Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8 } from "../assets";
+
+// Menu
+export const navigation = [ { id: "0", title: "Serviços", url: "#servicos", }, { id: "1", title: "Sobre nós", url: "#sobrenos", }, { id: "2", title: "Testemunhos", url: "#testemunhos", }, { id: "3", title: "Contactos", url: "#contactos", }
+];
+
+// Serviços
+export const roadmap = [
+  {
+    id: "0",
+    title: "Metodologia Humanoterapeuta",
+    text: "Esta metodologia consiste num bloco de tratamentos que abrange todos os campos vibracionais: espiritual, mental e emocional. Na primeira consulta é feita a avaliação na mesa Quântica, que nos indica as sessões necessárias para cada campo. As técnicas utilizadas são: Humanometria, Taquions-Toc, TDR- Terapia de Desbloqueio Emocional, Regressão, EFT- Emotional Freedom Techniques e Baralho Terapêutico. Permita-se e surpreenda-se com verdadeiros “insights”, que o conduzem fluidamente a uma expansão de consciência e real compreensão de si mesmo.",
+    date: "45€",
+    status: "online",
+    imageUrl: serviceOne,
+    colorful: true,
+  },
+  {
+    id: "1",
+    title: "Registos Akashicos",
+    text: "Consulta de abertura de Registos Akashicos, que são os registos de memória onde a alma guarda todas as experiências desta e de outras vidas. Nesta abertura são-nos explanadas várias lições que a alma traz para superar nesta encarnação, assim como as habilidades e talentos pessoais, qualidades, bloqueios de chakras, o alinhamento com a força divina e bloqueios. Por norma, os guias trazem-nos qualquer informação que estamos prontos a consciencializar e desta forma prossigamos ao seu desenvolvimento e evolução.",
+    date: "45€",
+    status: "online",
+    imageUrl: serviceTwo
+  },
+  {
+    id: "2",
+    title: "Consulta Astrológica",
+    text: "Desenho e interpretação de mapas astrológicos, os quais nos revelam qual a nossa genuína essência, quais os atributos, dons e desafios a desenvolver. São analisadas todas as áreas: desenvolvimento pessoal, carreira, relações, saúde, finanças e propósito de vida.  Nos mapas de Revolução Solar, analisamos quais as tendências energéticas, as áreas e temas a desenvolver e onde estão os maiores desafios.",
+    date: "45€",
+    status: "online",
+    imageUrl: serviceThree,
+  },
+  {
+    id: "3",
+    title: "Harmonizações",
+    text: "Essenciais ao nosso equilíbrio e bem-estar, as harmonizações são o verdadeiro “check-up” energético. Escolha a sua: Harmonização Radionica, Harmonização Quantica ou Pêndulo Hebreu.",
+    date: "36€",
+    status: "online",
+    imageUrl: serviceFour,
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    backgroundUrl: clienteCard,
+    imageUrl: logoSmall,
+    name: 'Ana Soares',
+    position: 'Marketing Director at GreenLeaf',
+    backgroundImage:'./src/assets/logo-small.png',
+    img: cliente1,
+    review:'Muito grata pelo tratamento, pois segui sempre apoiada, e venci os meus ataques de pânico. Desbloqueie emoções e lembranças que nem sabia que existiam mais. E o melhor de tudo, o aprendizado que pratico e me acompanha no dia a dia.',
+  },
+  {
+    id: "1",
+    backgroundUrl: clienteCard,
+    imageUrl: logoSmall,
+    light: true,
+    name: 'Anzhelika Ishkova',
+    position: 'Marketing Director at GreenLeaf',
+    backgroundImage:'./src/assets/logo-small.png',
+    img: cliente2,
+    review:'Desde a harmonização às técnicas de metodologia. É toda uma descoberta de autoconhecimento, expansão e reencontro comigo mesma. Recomendo a quem busca um novo olhar sobre a vida pois este processo transformou a minha forma de ver o mundo!',
+  },
+  {
+    id: "2",
+    backgroundUrl: clienteCard,
+    imageUrl: logoSmall,
+    name: 'Wander Pardini',
+    position: 'Marketing Director at GreenLeaf',
+    backgroundImage:'./src/assets/logo-small.png',
+    img: cliente3,
+    review:'Iniciei o tratamento humano terapêutico com a Catarina, reticente, pois era muito cético, logo na 1ª sessão despertou me uma curiosidade de saber o que mais iria abrir a minha mente. Então entendi o sentido de querer estar iluminado.',
+  },
+  {
+    id: "3",
+    backgroundUrl: clienteCard,
+    imageUrl: logoSmall,
+    light: true,
+    name: 'António Graça',
+    position: 'Marketing Director at GreenLeaf',
+    backgroundImage:'./src/assets/logo-small.png',
+    img: cliente4,
+    review:'Fiz os registos Akashicos com a Soraia, muito na curiosidade… e a curiosidade levou-me a muitas informações e entendimentos verdadeiramente surpreendentes. Foi um virar de chave para a vida! Ainda bem que sou curioso!',
+  },
+  {
+    id: "4",
+    backgroundUrl: clienteCard,
+    imageUrl: logoSmall,
+    name: 'Nuno Santos',
+    position: 'Marketing Director at GreenLeaf',
+    backgroundImage:'./src/assets/logo-small.png',
+    img: cliente5,
+    review:'Quando iniciei as sessões de terapia, estava à procura de algo que fosse mais profundo. Logo nas primeiras consultas, percebi uma leveza inesperada. É como se, aos poucos, camadas fossem saindo e eu pudesse ver com clareza quem realmente sou.',
+  },
+  {
+    id: "5",
+    backgroundUrl: clienteCard,
+    imageUrl: logoSmall,
+    name: 'Lourenço Caetano',
+    position: 'Marketing Director at GreenLeaf',
+    backgroundImage:'./src/assets/logo-small.png',
+    img: cliente6,
+    review:'Sempre fui prático e focado em resultados. Esta experiência de autodescoberta foi surpreendente, abrindo-me portas que eu nem sabia que existiam. Libertei-me de velhos padrões e, acima de tudo, aprendi a confiar em mim mesmo.',
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+// export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Photo generating",
+  "Photo enhance",
+  "Seamless Integration",
+];
+
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const servicesIconsCatarina = [
+  {
+    id: "0",
+    link: "https://www.facebook.com/catanouk66",
+    name: "facebook",
+    icon: facebook,
+  },
+  {
+    id: "1",
+    link: "https://www.instagram.com/hamanduniverso_catanouk66/",
+    name: "instagram",
+    icon: instagram,
+  },
+  {
+    id: "2",
+    link: "tel:+351915825671",
+    name: "phone",
+    icon: disc02,
+  },
+  {
+    id: "3",
+    link: "https://wa.me/+351915825671",
+    name: "whatsapp",
+    icon: chromecast,
+  },
+  {
+    id: "4",
+    link: "mailto:hamanduniverso@gmail.com",
+    name: "email",
+    icon: sliders04,
+  }
+];
+
+export const servicesIconsSoraia = [
+  {
+    id: "0",
+    link: "https://www.facebook.com/alexa.pereira.5667/",
+    name: "facebook",
+    icon: facebook,
+  },
+  {
+    id: "1",
+    link: "https://www.instagram.com/soraialexa.pereira/",
+    name: "instagram",
+    icon: instagram,
+  },
+  {
+    id: "2",
+    link: "tel:+34644661591",
+    name: "phone",
+    icon: disc02,
+  },
+  {
+    id: "3",
+    link: "https://wa.me/+34644661591",
+    name: "whatsapp",
+    icon: chromecast,
+  },
+  {
+    id: "4",
+    link: "mailto:hamanduniverso@gmail.com",
+    name: "email",
+    icon: sliders04,
+  }
+];
+
+
+
+export const collabText = "Fruto de duas jornadas transformadoras de autoconhecimento, estes projetos unem-se pelo divino, amor e luz, apoiando a humanidade na autocura e expansão de consciência. Todos os nosso serviços são, fundamentados no amor, na luz e no divino e visam proporcionar uma experiência transformadora para aqueles que buscam compreender-se mais profundamente, superar bloqueios e viver com maior conexão interior.";
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Seamless Integration",
+    text: collabText,
+  },
+  {
+    id: "1",
+    title: "Smart Automation",
+  },
+  {
+    id: "2",
+    title: "Top-notch Security",
+  },
+];
+
+export const collabApps = [
+  { id: "0", title: "Icon1", icon: Icon1, width: 40, height: 40 },
+  { id: "1", title: "Icon2", icon: Icon2, width: 40, height: 40 },
+  { id: "2", title: "Icon3", icon: Icon3, width: 40, height: 40 },
+  { id: "3", title: "Icon4", icon: Icon4, width: 40, height: 40 },
+  { id: "4", title: "Icon5", icon: Icon5, width: 40, height: 40 },
+  { id: "5", title: "Icon6", icon: Icon6, width: 40, height: 40 },
+  { id: "6", title: "Icon7", icon: Icon7, width: 40, height: 40 },
+  { id: "7", title: "Icon8", icon: Icon8, width: 40, height: 40 },
+  
+];
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Basic",
+    description: "AI chatbot, personalized recommendations",
+    price: "0",
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+  // {
+  //   id: "1",
+  //   title: "Premium",
+  //   description: "Advanced AI chatbot, priority support, analytics dashboard",
+  //   price: "9.99",
+  //   features: [
+  //     "An advanced AI chatbot that can understand complex queries",
+  //     "An analytics dashboard to track your conversations",
+  //     "Priority support to solve issues quickly",
+  //   ],
+  // },
+  // {
+  //   id: "2",
+  //   title: "Enterprise",
+  //   description: "Custom AI chatbot, advanced analytics, dedicated account",
+  //   price: null,
+  //   features: [
+  //     "An AI chatbot that can understand your queries",
+  //     "Personalized recommendations based on your preferences",
+  //     "Ability to explore the app and its features without any cost",
+  //   ],
+  // },
+];
+
+
+export const socials = [
+  {
+    id: "0",
+    title: "Intagram",
+    iconUrl: instagram,
+    url: "https://www.instagram.com/hamanduniverso_catanouk66/",
+  },
+  {
+    id: "1",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "https://www.facebook.com/catanouk66/",
+  },
+  {
+    id: "2",
+    title: "Tiktok",
+    iconUrl: tiktok,
+    url: "http://tiktok.com/@hamanduniverso/",
+  },
+  // {
+  //   id: "3",
+  //   title: "Discord",
+  //   iconUrl: discordBlack,
+  //   url: "#",
+  // },
+  // {
+  //   id: "4",
+  //   title: "Telegram",
+  //   iconUrl: telegram,
+  //   url: "#",
+  // },
+];
